7f14ab9118022d4deb8d036daf4dce9ae836ba3d
DDarkInferno <luxiadev@gmail.com>
2017-09-17 19:44:24 +1000

    Edit

--- PirateDark Mc Plugins/source/darkpirate/plugins/healingbeacons/Main.java
+++ PirateDark Mc Plugins/source/darkpirate/plugins/healingbeacons/Main.java
@@ -1,7 +1,18 @@
 package darkpirate.plugins.healingbeacons;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+	 
+    @Override
+    public void onEnable() {
+       getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "\n\nDuels have being enabled\n\n");
+    }
+   
+    @Override
+    public void onDisable() {
+       getServer().getConsoleSender().sendMessage(ChatColor.RED + "\n\nDuels has being disabled\n\n");
+    }
 
 }
