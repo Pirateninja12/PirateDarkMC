428bbdec9d915a7f5d1a67d1bb2d5bc199fe7771
DDarkInferno <luxiadev@gmail.com>
2017-09-17 21:26:54 +1000

    Edit

--- PirateDark Mc Plugins/src/darkpirate/plugins/healingbeacons/Events.java
+++ PirateDark Mc Plugins/src/darkpirate/plugins/healingbeacons/Events.java
@@ -11,19 +11,18 @@ import org.bukkit.event.player.PlayerInteractEvent;
 
 public class Events implements Listener {
 
-	@SuppressWarnings("static-access")
 	@EventHandler
 	public void onInteract(PlayerInteractEvent event) {
 		Player player = event.getPlayer();
 		Action action = event.getAction();
 		Block block = event.getClickedBlock();
 		
-		if(action.equals(action.RIGHT_CLICK_BLOCK)) {
-			if(block.equals(Material.BEACON)){
-				if(player.hasPermission("beacon.heal")) {
+		if(action.equals(Action.LEFT_CLICK_BLOCK)) {
+			if(block.equals(Material.SLIME_BLOCK)){
+				if(player.hasPermission("slime.heal")) {
 					if(player.getHealth() != 20) {
 						player.setHealth(player.getHealth() + 1);
-						player.sendMessage(ChatColor.GREEN + "You have being healed because you clicked a block at " + block.getX() + block.getY() + block.getZ());
+						player.sendMessage(ChatColor.GREEN + "You have being healed because you clicked a slime block at " + block.getX() + block.getY() + block.getZ());
 						player.sendMessage(ChatColor.BLUE + "You have gained half a heart");
 					}else{
 						player.sendMessage(ChatColor.RED + "You have full health");
