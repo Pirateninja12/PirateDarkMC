669060fc898857cf48be7705f88806ab9cd5a5a6
Pirateninja12 <daemienz@gmail.com>
2017-09-17 19:56:41 -0700

    Added hunger to punching the beacons. Do you know how to go about taking away points of health/hunger if they spam the action without the needed permissions?

--- PirateDark Mc Plugins/src/darkpirate/plugin/hb/Events/Events.java
+++ PirateDark Mc Plugins/src/darkpirate/plugin/hb/Events/Events.java
@@ -23,20 +23,38 @@ public class Events implements Listener {
 				if(player.hasPermission("beacon.heal")) { 
 					if(player.getHealth() != 20) {
 						player.setHealth(player.getHealth() + 1);
-						player.sendMessage(ChatColor.DARK_GREEN + "You have being healed because you punched a beacon" + ChatColor.LIGHT_PURPLE + " X: " + ChatColor.WHITE + block.getX() + ChatColor.LIGHT_PURPLE + " Y: " + ChatColor.WHITE + block.getY() + ChatColor.LIGHT_PURPLE + " Z: " + ChatColor.WHITE + block.getZ());
-						player.sendMessage(ChatColor.BLUE + "You have gained half a heart");
+						player.sendMessage(ChatColor.DARK_GREEN + "You have been healed because you punched a beacon." + ChatColor.LIGHT_PURPLE + " X: " + ChatColor.WHITE + block.getX() + ChatColor.LIGHT_PURPLE + " Y: " + ChatColor.WHITE + block.getY() + ChatColor.LIGHT_PURPLE + " Z: " + ChatColor.WHITE + block.getZ());
+						player.sendMessage(ChatColor.BLUE + "You have gained half a heart.");
 					}else{
-						player.sendMessage(ChatColor.RED + "You have full health");
+						player.sendMessage(ChatColor.RED + "You have full health.");
 					}
 				}else{
-					player.sendMessage(ChatColor.RED + "You do not have permission to punch this beacon and get healed");
+					player.sendMessage(ChatColor.RED + "You do not have permission to punch this beacon and get healed.");
 				}
 				
 			}
 		}else if(action.equals(Action.RIGHT_CLICK_BLOCK)) {
+			if(block.getType().equals(Material.BEACON)){
+				if(player.hasPermission("beacon.heal")) {
+					if(player.getHunger() != 20) {
+						player.setHunger(player.getHunger() + 1);
+						player.sendMessage(ChatColor.DARK_GREEN + "Your hunger has been replenished because you punched a beacon." + ChatColor.LIGHT_PURPLE + " X: " + ChatColor.WHITE + block.getX() + ChatColor.LIGHT_PURPLE + " Y: " + ChatColor.WHITE + block.getY() + ChatColor.LIGHT_PURPLE + " Z: " + ChatColor.WHITE + block.getZ());
+						player.sendMessage(ChatColor.BLUE + "You have gained a hunger point.");
+					}else {
+						player.sendMessage(ChatColor.RED + "You have no need for any more food.");
+					}
+				}else{
+					player.sendMessage(ChatColor.RED + "You do not have permmission to punch this beacon and gain hunger.");
+					
+				}
+			}
 			
 		}
 		
 	}
 
 }
+/*Fixed some grammar and spelling issues with in the printed texts
+ *Added the hunger to right click...did not know what color so I decided to keep it stream lined and run with the same colors you had.
+ *
+ */
